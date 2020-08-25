4a5ee7b58880964a231351f7819406208d2a7016
Hunter Gilliam <huntergilliam03@gmail.com>
2020-08-25 12:43:38 -0500

    Initial Commit

---
+++ UI_FinanceMobileApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UI_FinanceMobileApp
+//
+//  Created by Hunter Gilliam on 8/25/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
